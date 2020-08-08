89631599321d7a97dbad32d6ddecc4149062ac86
dragonfly161518 <por.nattapat.01@gmail.com>
2020-08-09 02:54:43 +0700

    set Dimension according to innerHeight and aspect

--- src/DesignCanvas/DesignCanvas.tsx
+++ src/DesignCanvas/DesignCanvas.tsx
@@ -1,15 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { fabric } from "fabric";
 import { useCanvas } from "../Canvas";
 
+const ratio = [9, 16];
+
 const DesignCanvas = () => {
   const { createCanvas } = useCanvas();
+  const [dimension, setDimension] = useState<number[]>([0, 0]);
+  const isFirst = useRef(true);
+
   useEffect(() => {
-    const canvas = new fabric.Canvas("canvas");
-    createCanvas(canvas);
+    setDimension([
+      (window.innerHeight / ratio[1]) * ratio[0],
+      window.innerHeight,
+    ]);
   }, []);
 
-  return <canvas id="canvas"></canvas>;
+  useEffect(() => {
+    if (isFirst.current) {
+      isFirst.current = false;
+      return;
+    }
+    const canvas = new fabric.Canvas("canvas");
+    createCanvas(canvas);
+    var rect = new fabric.Rect({
+      top: 100,
+      left: 100,
+      width: 60,
+      height: 70,
+      fill: "red",
+    });
+    canvas.add(rect);
+  }, [dimension]);
+
+  return (
+    <canvas id="canvas" width={dimension[0]} height={dimension[1]}></canvas>
+  );
 };
 
 export default DesignCanvas;
