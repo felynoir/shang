041d17d4815fdcab2d1c64dbd030d68d53d1db5f
dragonfly161518 <por.nattapat.01@gmail.com>
2020-08-10 22:38:46 +0700

    add modal

--- src/Tools/AddBackground.tsx
+++ src/Tools/AddBackground.tsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import AddPhotoSVG from "../assets/icons/add_photo_alternate-black-18dp.svg";
 import { useCanvas } from "../hooks";
+import Modal from "../Modal/Modal";
 
 const AddBackground = () => {
   const { addImage } = useCanvas();
+  const [opened, setOpened] = useState<boolean>(false);
 
-  const handleAction = () => {
-    alert("action");
+  const handleAction = (opened) => {
+    setOpened(opened);
   };
 
   return (
-    <div onClick={handleAction}>
-      <img width="24" height="24" src={AddPhotoSVG} />
-    </div>
+    <>
+      <div
+        className="flex items-center justify-center rounded-full h-12 w-12 p-3 hover:bg-gray-200 cursor-pointer"
+        onClick={() => setOpened(true)}
+      >
+        <img width="24" height="24" src={AddPhotoSVG} />
+      </div>
+      <Modal opened={opened} setOpened={handleAction} />
+    </>
   );
 };
 
